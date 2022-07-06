f251e731b38ff458cd415e198c0e1ea4d8a6b043
KarinaRin <beketaeva.karina@gmail.com>
2022-07-06 18:29:40 +0300

    add validate in serializers

--- comment/serializers.py
+++ comment/serializers.py
@@ -4,6 +4,13 @@ from .models import *
 
 class CommentSerializer(serializers.ModelSerializer):
 
+    def validate(self, data):
+        if data['content_type'] not in ContentType.objects.all():
+            raise serializers.ValidationError("This entity doesn't exist yet!")
+        if not data['content_type'].model_class().objects.filter(id=data['object_id']):
+            raise serializers.ValidationError("This entity's object doesn't exist yet!")
+        return data
+
     class Meta:
         model = Comment
         fields = ['id', 'content_type', 'object_id', 'parent', 'created_at', 'text', 'user']
