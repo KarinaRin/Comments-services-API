c1bdd3c1e2b32fe5f532605428ec476534ac4251
KarinaRin <beketaeva.karina@gmail.com>
2022-07-12 06:40:48 +0300

    add superuser

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
