62a34fb1ddf642bfd09144538e6416fb31d5026a
KarinaRin <beketaeva.karina@gmail.com>
2022-07-12 22:22:29 +0300

    some change

--- comment/serializers.py
+++ comment/serializers.py
@@ -5,6 +5,7 @@ from .models import *
 class CommentSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
+
         if data['content_type'] not in ContentType.objects.all():
             raise serializers.ValidationError("This entity doesn't exist yet!")
         if not data['content_type'].model_class().objects.filter(id=data['object_id']):
