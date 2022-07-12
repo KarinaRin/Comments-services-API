9bf0eb0c88496e8db1c2984c908d4f5e2061f9fb
KarinaRin <beketaeva.karina@gmail.com>
2022-07-12 06:38:21 +0300

    add postgresql

--- servicecomments/settings.py
+++ servicecomments/settings.py
@@ -61,7 +61,7 @@ WSGI_APPLICATION = 'servicecomments.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.postgresql',
     }
 }
 
